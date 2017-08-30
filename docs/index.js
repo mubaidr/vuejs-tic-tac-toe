9f7155abc0bc0c54ca7500b4b508ec9b20b8950d
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-08-30 15:40:48 +0500

    setup data

--- docs/index.js
+++ docs/index.js
@@ -3,10 +3,45 @@ Vue.config.productionTip = false
 // eslint-disable-next-line
 var myApp = new Vue({
   el: '#app',
-  data: {},
+  data: {
+    screen: 0,
+    turn: '',
+    playerData: {
+      one: {
+        symbol: 'X',
+        score: 0
+      },
+      two: {
+        symbol: 'O',
+        score: 0
+      }
+    },
+    isComputer: true
+  },
   created () {},
   mounted () {},
   watch: {},
   computed: {},
-  methods: {}
+  methods: {
+    setPlayerSymbol (symbol) {
+      this.playerData.one.symbol = symbol
+      this.playerData.two.symbol = symbol === 'X' ? 'O' : symbol
+      this.skipScreen()
+    },
+    setGameType (type) {
+      this.isComputer = type
+      this.skipScreen()
+    },
+    skipScreen () {
+      this.screen += 1
+    },
+    resetScreen () {
+      this.screen = 0
+      this.isComputer = true
+
+      Object.keys(this.playerData).forEach((type) => {
+        this.playerData[type].score = 0
+      })
+    }
+  }
 })
